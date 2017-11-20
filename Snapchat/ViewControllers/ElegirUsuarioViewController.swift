00ebb92a4b29cd64137d81ba1ea65381c65551b8
Mac  Tecsup <claudia.cordova@tecsup.edu.pe>
2017-11-20 17:42:21 -0500

    creación de snaps en firebase

--- Snapchat/ViewControllers/ElegirUsuarioViewController.swift
+++ Snapchat/ViewControllers/ElegirUsuarioViewController.swift
@@ -12,6 +12,8 @@ import Firebase
 class ElegirUsuarioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var usuarios : [Usuario] = []
+    var imagenURL = ""
+    var descrip = ""
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -38,5 +40,11 @@ class ElegirUsuarioViewController: UIViewController, UITableViewDataSource, UITa
         let usuario = usuarios[indexPath.row]
         cell.textLabel?.text = usuario.email
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let usuario = usuarios[indexPath.row]
+        let snap = ["from":usuario.email, "descripcion":descrip, "imagenURL":imagenURL]
+        Database.database().reference().child("usuarios").child(usuario.uid).child("snaps").childByAutoId().setValue(snap)
     }
 }
