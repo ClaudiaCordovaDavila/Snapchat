bff7d63feff96ef46d011007473b5cb283be8054
Mac  Tecsup <claudia.cordova@tecsup.edu.pe>
2017-11-27 17:37:58 -0500

    corrigiendo error al enviar snap sin imagen

--- Snapchat/ViewControllers/ImagenViewController.swift
+++ Snapchat/ViewControllers/ImagenViewController.swift
@@ -21,13 +21,14 @@ class ImagenViewController: UIViewController, UIImagePickerControllerDelegate,  
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        // Do any additional setup after loading the view.
+        elegirContactoBoton.isEnabled = false
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = image
         imageView.backgroundColor = UIColor.clear
+        elegirContactoBoton.isEnabled = true
         imagePicker.dismiss(animated: true, completion: nil)
     }
 
